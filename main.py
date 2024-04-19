ae7ef79bb608bc4a99a205ddbcc8fa1c6f8bf059
duckduckgrayduck <102841251+duckduckgrayduck@users.noreply.github.com>
2024-04-19 14:26:39 -0500

    First run

--- main.py
+++ main.py
@@ -1,38 +1,34 @@
 """
-This is a hello world add-on for DocumentCloud.
-
-It demonstrates how to write a add-on which can be activated from the
-DocumentCloud add-on system and run using Github Actions.  It receives data
-from DocumentCloud via the request dispatch and writes data back to
-DocumentCloud using the standard API
+Simple DocumentCloud Add-On that tags documents
+with the OCR engine used on said documents. 
 """
-
+import requests
 from documentcloud.addon import AddOn
 
 
-class HelloWorld(AddOn):
-    """An example Add-On for DocumentCloud."""
+class OCRTagger(AddOn):
+    """Tags documents with OCR engine"""
 
     def main(self):
-        """The main add-on functionality goes here."""
-        # fetch your add-on specific data
-        name = self.data.get("name", "world")
-
-        self.set_message("Hello World start!")
-
-        # add a hello note to the first page of each selected document
+        """ For each document finds the ocr value from the json text and tags """
         for document in self.get_documents():
-            # get_documents will iterate through all documents efficiently,
-            # either selected or by query, dependeing on which is passed in
-            document.annotations.create(f"Hello {name}!", 0)
-
-        with open("hello.txt", "w+") as file_:
-            file_.write("Hello world!")
-            self.upload_file(file_)
-
-        self.set_message("Hello World end!")
-        self.send_mail("Hello World!", "We finished!")
+            try:
+                json_text_url = f"{document.asset_url}documents/{document.id}/{document.slug}.txt.json"
+                response = requests.get(json_text_url, timeout=10)
+                json_data = response.json()
+                ocr_value = json_data["pages"][0]["ocr"]
+            except:
+                ocr_value = "None"
+            ocr_mapping = {
+                "tess4": "tesseract",
+                "textract_force": "textract",
+                "azuredi": "azure",
+                "googlecv": "google",
+                "doctr": "doctr",
+                "None": "None",
+            }
+            document.data["ocr_engine"] = ocr_mapping.get(ocr_value)
 
 
 if __name__ == "__main__":
-    HelloWorld().main()
+    OCRTagger().main()
